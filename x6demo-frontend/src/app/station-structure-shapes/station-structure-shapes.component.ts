773375e002a00a226311522ce4f12845383d506b
RKUZNETSOV <rkuznetsov@dynamicsun.ru>
2023-04-27 12:49:37 +0300

    Изображения потребителя и резервуара

--- x6demo-frontend/src/app/station-structure-shapes/station-structure-shapes.component.ts
+++ x6demo-frontend/src/app/station-structure-shapes/station-structure-shapes.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ShapeDefinitionConf } from './shape-definition.directive';
+import { PortManager } from '@antv/x6/lib/model/port';
 
 const c = (v: ShapeDefinitionConf) => v;
 const defSize = {
   width: 100,
   height: 180,
+};
+
+const absPortGroup: PortManager.GroupMetadata = {
+  position: 'absolute',
+  attrs: { circle: { r: 6 } },
 };
 @Component({
   selector: 'app-station-structure-shapes',
@@ -19,19 +25,18 @@ export class StationStructureShapesComponent {
       width: 120,
       height: 30,
       ports: {
-        groups: {
-          g: {
-            position: 'absolute',
-            attrs: { circle: { r: 6 } },
-          },
-        },
+        groups: { g: absPortGroup },
         items: [{ id: 'main', group: 'g', args: { x: '100%', y: '50%' } }],
       },
     }),
     target: c({
       shape: 'station-structure-target',
-      ...defSize,
-      ports: [{ id: 'i1' }],
+      width: 150,
+      height: 30,
+      ports: {
+        groups: { g: absPortGroup },
+        items: [{ id: 'i1', group: 'g', args: { x: 0, y: '50%' } }],
+      },
     }),
     pump3i1o: c({
       shape: 'station-structure-pump-3i-1o',
@@ -40,8 +45,15 @@ export class StationStructureShapesComponent {
     }),
     tank1i1o: c({
       shape: 'station-structure-tank-1i-1o',
-      ...defSize,
-      ports: [{ id: 'i1' }, { id: 'main' }],
+      width: 320,
+      height: 180,
+      ports: {
+        groups: { g: absPortGroup },
+        items: [
+          { id: 'i1', args: { x: 0, y: 165 }, group: 'g' },
+          { id: 'main', args: { x: '100%', y: 165 }, group: 'g'  },
+        ],
+      },
     }),
     sep1i2o1: c({
       shape: 'station-structure-sep-1i-2o-1rem',
